865b02c65d2f6f5aeab1fb53ecaacb01f44837c9
AleksandrsVasilevskis171RDB362 <125203034+AleksandrsVasilevskis171RDB362@users.noreply.github.com>
2023-03-19 19:07:09 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,25 +1,114 @@
 # python3
+from collections import deque
+import heapq
 
+swaps = []
 
 def build_heap(data):
-    swaps = []
+    count = 0
+    
+
+    dataLenth = len(data)
+    
+    check = False
+    
+    while(check ==False):
+        i = dataLenth - 1
+        check = True
+        while (i >= 0):
+
+            left = 2*i+1
+            right = 2*i+2
+
+            if left > dataLenth-1 and right > dataLenth-1:
+
+                pass
+            elif left <= dataLenth-1 and right > dataLenth-1:
+                x = data[i]
+                y = data[left]
+                check = False
+                if x > y:
+                    data[i] = y
+                    data[left] = x
+                    count= count+1
+                    string = str(i)+" "+str(left)
+                   # print(count)
+
+                    swaps.append(string)
+                pass
+                
+
+            elif left <= dataLenth-1 and right <= dataLenth-1:
+               
+                x = data[i]
+                y = data[left]
+                z = data[right]
+
+                if x > y and y<z:
+                    check = False
+                    data[i] = y
+                    data[left] = x
+                    count =count+1
+                    string = str(i)+" "+str(left)
+                    #print(count)
+                    swaps.append(string)
+                    pass
+                elif x > z and z < y:
+                    check = False
+                    data[i] = z
+                    data[right] = x
+                    count=count+1
+                    string = str(i)+" "+str(right)
+                    #print(count)
+                    swaps.append(string)
+                pass
+
+            i=i-1
+    swaps.insert(0,str(count))
+    
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    
+    #return data
+    
     return swaps
 
 
-def main():
+def main(): 
+    #print("hi")
+    x = 3 
+    #print("hi1")
+    y =[9,8,7,6,5,4,3,2,1]
+    #print("hi2")
+    #build_heap(y)
+   # print("hi3")
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    #print(listArray)
 
+    check =input()
+    for i in check:
+        if i == "I":
+            n = int(input())
+            data = list(map(int, input().split()))
+            listArray=build_heap(data)
+            x=' '.join(listArray)
+            print(x)
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+        elif i == "F":
+             url = r"C:\Users\ReFoxiK\Downloads\convert-array-into-heap-AleksandrsVasilevskis171RDB362-main\convert-array-into-heap-AleksandrsVasilevskis171RDB362-main\tests\04"
+             f = open(url)
+             readFileArray = f.read().splitlines()
+             elementCount=int(readFileArray[0])
+             arrList = readFileArray[1]
+             arrList = arrList.split()
+             arrListInt = []
+             for i in arrList:
+                arrListInt.append(int(i)) 
+
+             build_heap(arrListInt)
+        else:
+            print("ERROR")
+    
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
