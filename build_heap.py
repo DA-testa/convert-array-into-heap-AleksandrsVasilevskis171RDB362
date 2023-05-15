e59df41a39edca1561375fcff7431b3eecb36689
AleksandrsVasilevskis171RDB362 <125203034+AleksandrsVasilevskis171RDB362@users.noreply.github.com>
2023-05-15 15:03:47 +0300

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -146,6 +146,8 @@ def main():
             while i<len(swops):
                 if i +1 <swops:
                     print (swops[i],swops[i+1])
+                else:
+                    print(swops[i])
                 i = i + 1
             #listArray=build_heap(data,n)
             #x=' '.join(listArray)
@@ -168,6 +170,9 @@ def main():
              while i<len(swops):
                 if i + 1< len(swops)     
                     print (swops[i],swops[i+1])
+                else:
+                    print(swops[i])
+                    
                 i = i + 1
         else:
             print("ERROR")
